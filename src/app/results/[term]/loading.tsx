a4646f61aa651dc439561eab5e205cdc1a6db22a
VAHAB <vahab.azimzadeh@gmail.com>
2025-08-11 14:40:30 +0200

    Remove Loading component from results page

--- src/app/results/[term]/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="w-full flex justify-center items-center">
-      <div className="animate-spin rounded-full h-20 w-20 border-b-5 bg-transparent border-gray-200"></div>
-    </div>
-  );
-}
